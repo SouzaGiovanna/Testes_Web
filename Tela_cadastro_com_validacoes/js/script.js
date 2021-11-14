62ea35665e4cc715ed5296fe96532fe3d0f5d9e3
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2021-11-14 09:22:22 -0300

    formulário pronto

--- Tela_cadastro_com_validacoes/js/script.js
+++ Tela_cadastro_com_validacoes/js/script.js
@@ -1,10 +1,25 @@
 class Validator{
     constructor(){
-        this.validations = ['data-min-length',];
+        this.validations = [
+            'data-required',
+            'data-min-length',
+            'data-max-length',
+            'data-email-validate',
+            'data-only-letters',
+            'data-equal',
+            'data-password-validate'
+        ];
     }
 
     //iniciar a validação de todos os campos
     validate(form){
+        //resgata todas as validações
+        let currentValidations = document.querySelectorAll('form .error-validation');
+
+        if(currentValidations.length > 0){
+            this.cleanValidations(currentValidations);
+        }
+
         //pegar os inputs
         let inputs = form.getElementsByTagName('input');
 
@@ -40,17 +55,98 @@ class Validator{
         }
     }
 
+    //verifica se um input passou do limite de caracteres
+    maxlength(input, maxValue){
+        let inputLength = input.value.length;
+
+        let errorMessage = `O campo pode ter no máximo ${maxValue} caracteres`;
+
+        if(inputLength > maxValue){
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    //verifica se o input é obrigatório
+    required(input){
+        let inputValue = input.value;
+
+        if(inputValue === ''){
+            let errorMessage = 'Esse campo é obrigatório';
+
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    //valida o formato do email
+    emailvalidate(input){
+        let regex = /\S+@\S+\.\S+/;
+
+        let email = input.value;
+
+        let errorMessage = 'Insira um e-mail no padrão exemplo@email.com';
+
+        if(!regex.test(email)){
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    //valida se o campo tem apenas letras
+    onlyletters(input){
+        let regex = /^[A-Za-z]+$/;
+
+        let inputValue = input.value;
+
+        let errorMessage = `Este campo não aceita números nem caracteres especiais`;
+
+        if(!regex.test(inputValue)){
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    //valida se dois campos são iguais
+    equal(input, inputName){
+        console.log("foi");
+        let inputToCompare = document.getElementsByName(inputName)[0];
+
+        let errorMessage = `Este campo precisa estar igual ao campo ${inputName}`;
+
+        if(input.value != inputToCompare.value){
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    //valida campo de Senha
+    passwordvalidate(input){
+        let regex = /[A-Za-z\d@$!%*#?&]/;
+
+        let errorMessage = `Sua senha precisa ter caracteres maiúsculos, minusculos, numéricos e especiais`;
+
+        if(regex.test(input.value)){
+            this.printMessage(input, errorMessage);
+        }
+    }
+
     //método para imprimir mensagens de erro na tela
     printMessage(input, msg){
-        let template = document.querySelector('.error-validation').cloneNode(true);
+        //quantidade de erros
+        let errorsQtd = input.parentNode.querySelector('.error-validation');
 
-        template.textContent = msg;
+        if(errorsQtd === null){
+            let template = document.querySelector('.error-validation').cloneNode(true);
 
-        let inputParent = input.parentNode;
+            template.textContent = msg;
 
-        template.classList.remove('template');
+            let inputParent = input.parentNode;
 
-        inputParent.appendChild(template);
+            template.classList.remove('template');
+
+            inputParent.appendChild(template);
+        }
+    }
+
+    //limpa as validações da tela
+    cleanValidations(validations){
+        validations.forEach(el => el.remove());
     }
 }
 
